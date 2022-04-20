ccae33e615b71e12d700716a32ec01ef2265caca
bin549 <bin549@qq.com>
2022-04-20 14:45:57 +0800

    2

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,8 +6,9 @@ import { localGet, localRemove, localSet } from './auth'
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_BASE_API,
-    timeout: 1000 * 12
-}
+    timeout: 1000 * 12 }
 )
+
+
 
 export default instance
